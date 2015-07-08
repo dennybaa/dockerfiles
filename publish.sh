6efce6931ffe8760b28d3a5a5d30f4faa58ea68f
Denis Baryshev <dennybaa@gmail.com>
2015-07-08 20:50:43 +0300

    publish rework

--- publish.sh
+++ publish.sh
@@ -1,22 +1,30 @@
 #!/bin/sh
 
-# This script builds/rebuilds containers
+# This script builds/rebuilds containers and pushes them to remote registries.
+# Examples:
 #
+# ./publish.sh -p quay.io/dennybaa/ droneunit-ubuntu              => quay.io/dennybaa/droneunit-ubuntu
+# ./publish.sh --no-push -p quay.io/dennybaa/ $(ls -d */)         => builds all containers
+# ./publish.sh -p quay.io/dennybaa/ droneunit/Dockerfile.debian   => quay.io/dennybaa/droneunit-debian
 #
 
 BUILD_OPTS=
+TEMP=`getopt -o p: -l no-cache,no-push,rm -- "$@"`
+parse_status=$?
 
 usage() {
-  echo "Usage: $0 [--no-cache --no-push --rm] [-p registry prefix path ] container container ..."
+  echo "Usage: $0 [--no-cache --no-push --rm] [-p registry prefix path ] container[/Dockerfile.namesuffix] container[/Dockerfile.namesuffix] ..."
   echo "\t --no-cache - Do not use cache when building the image"
   echo "\t --no-push  - Do not publish into remote repository"
   echo "\t --rm       - Remove intermediate containers after a successful build" 
   echo
-  echo "\t -p         - Path to remote registry including prefix, ex: quay.io/myusername/" 
+  echo "\t -p         - Path to remote registry including prefix, ex: quay.io/myusername/"
+  echo
+  echo "\t Containers found with path suffix (container/Dockerfile.debian) will be tagged using"
+  echo "\t special pattern. Specifically for [-p quay.io/myusername/ myapp/Dockerfile.debian],"
+  echo "\t the image will be tagged as quay.io/myusername/myapp-debian."
 }
 
-TEMP=`getopt -o p: -l no-cache,no-push,rm -- "$@"`
-parse_status=$?
 
 # parse check
 [ "0" != $parse_status ] && { echo && usage && exit $parse_status; }
@@ -32,6 +40,7 @@ while true ; do
       NO_PUSH=1; shift;;
     --rm)
       BUILD_OPTS="${BUILD_OPTS} --rm"; shift;;
+
     *) break;;
   esac
 done
@@ -40,11 +49,20 @@ done
 CONTAINERS=$@
 [ $(echo ${CONTAINERS} | wc -w) = 0 ] && { usage && exit 1; }
 
-# Execute build
-for c in $CONTAINERS; do
-  c=$(echo $c | sed -r 's/\/+$//')
-  docker build ${BUILD_OPTS} -t ${PREFIX_PATH}${c} ${c} || continue
+# Execute builds
+for cpath in $CONTAINERS; do
+  # get contianer base container[/Dockerfile.debian] => container
+  c=$(echo $cpath | cut -f1 -d\/)
+  # get optional /Dockerfile.debian without /
+  dockerfile=$(echo ${cpath#$c} | sed -r 's/\/+//')
+
+  if [ ! -z "$dockerfile" ]; then
+    BUILD_OPTS="${BUILD_OPTS} -f ${c}/${dockerfile}"
+    suffix="-$(echo $dockerfile | cut -f2 -d.)"
+  fi
+
+  docker build ${BUILD_OPTS} -t ${PREFIX_PATH}${c}${suffix} $c || continue
 
   # push is required
-  [ "$NO_PUSH" != 1 ] && docker push ${PREFIX_PATH}${c}
+  [ "$NO_PUSH" != 1 ] && docker push ${PREFIX_PATH}${c}${suffix}
 done
